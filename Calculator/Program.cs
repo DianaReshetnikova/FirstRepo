0357aac13ab0513e25179fbd7143f5b81b510364
DianaReshetnikova <i@dharchenko.ru>
2024-02-06 14:19:24 +0300

    Created initial project

---
+++ Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Transactions;
+
+while (true)
+{
+    Console.WriteLine("Input first value: ");
+    if (!Double.TryParse(Console.ReadLine(), out double leftValue))
+    {
+        Console.WriteLine("Incorrect first value. Try again");
+        continue;
+    }
+
+secondStep: Console.WriteLine("Input sign: '+', '-'");
+    char sign = Console.ReadKey().KeyChar;
+    if (sign != '+' && sign != '-')
+    {
+        Console.WriteLine("Incorrect sign. Try again");
+        goto secondStep;
+    }
+
+
+    Console.WriteLine();
+thirdStep: Console.WriteLine("Input second value: ");
+    if (!Double.TryParse(Console.ReadLine(), out double rightValue))
+    {
+        Console.WriteLine("Incorrect second value. Try again");
+        goto thirdStep;
+    }
+
+    
+}
